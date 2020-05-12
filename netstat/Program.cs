1163aa83ec19b57edc5f85f2e9f1050349050f46
Connor Baxter <ConnorBaxterSHU@users.noreply.github.com>
2020-05-13 00:39:41 +0100

    Added netstat

---
+++ netstat/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace netstat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\n");
+            Console.WriteLine("Active Connections");
+            Console.WriteLine("\n");
+            Console.WriteLine("  Proto          Local Address           Foriegn Address         State");
+            Console.WriteLine("\n\nNo foriegn addresses found. Computer is secure from hackers!");
+            Console.WriteLine("\n\n");
+#if DEBUG
+            Console.ReadLine();
+#endif
+        }
+    }
+}
